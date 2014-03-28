5b0cafc4b02b9263b6139718bf4f392d142c8e09
Darrell Pratt <darrell.pratt@gmail.com>
2014-03-28 14:05:19 -0500

    rabbit

---
+++ worker.js
@@ -0,0 +1,59 @@
+var amqp = require('amqp');
+var path = require('path');
+var fs = require('graceful-fs');
+var oboe = require('oboe');
+var q = require('q');
+
+var connection = amqp.createConnection({host: 'localhost'});
+
+connection.on('ready', function(){
+    connection.queue('task_queue', {autoDelete: false,
+                                    durable: true}, function(queue){
+
+        console.log(' [*] Waiting for messages. To exit press CTRL+C');
+
+        queue.subscribe({ack: true, prefetchCount: 1}, function(msg){
+            var body = msg.data.toString('utf-8');
+            console.log(" [x] Received %s", body);
+            setTimeout(function(){
+                console.log(" [x] Done");
+                queue.shift(); // basic_ack equivalent
+            }, (body.split('.').length - 1) * 10);
+        });
+    });
+});
+
+
+function findVals(file, id) {
+  var deferred = q.defer();
+  // console.log("function one");
+  console.log(file);
+  console.time("findVals");
+  oboe(fs.createReadStream(jsonDir + file, {
+    autoClose: true
+  })).on('node',
+      {
+        'selections': function(scheme){
+           //console.log(scheme);
+         },
+        '{nodeId type}': function(item, path, ancestors) {
+          if (item.type === 'GROUP_LEVEL_ITEM' && item.nodeId === id) {
+              console.log(item);
+              console.log('FILE: ' + file);
+              console.dir(path);
+              console.timeEnd("findVals");
+
+              // console.log('ANCESTORS_0: ' + ancestors[0]);
+              // console.dir('ANCESTORS: ' + ancestors);
+            }
+        }
+      // }).on('done', function (json) {
+      //   console.log('request completed');
+      //   console.timeEnd("findVals");
+
+      });
+      deferred.resolve();
+
+
+  return deferred.promise;
+}
